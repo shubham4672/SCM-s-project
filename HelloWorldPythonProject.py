c35694f9063ff186b48a89fbcfd67888f14c9769
Utkarsh Kohli <102008597+ImUKohli@users.noreply.github.com>
2022-05-25 13:12:25 +0530

    Hello Git

---
+++ HelloWorldPythonProject.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This program prints Hello, world!
+
+print('Hello World! This is our Group Project')
+
+
+# In[ ]:
+
+
+
+
---
+++ Readme-uk.txt
@@ -0,0 +1,6 @@
+Hello this is the Project made by our Group.
+Our group contributors are:-
+1. Utkarsh Kohli
+2. Shubham Sharma
+3. Jhalak
+4. Divyansh Sharma
